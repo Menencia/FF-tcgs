08c0dc0e0e999ed965d3bae52740526090fcb41a
Menencia <menencia@gmail.com>
2014-12-12 00:30:36 +0100

    Removing dist/ folder — use gulp to generate those files

--- dist/bootstrap.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-require(['cards/Jecht_1', 'cards/Red_XIII_1'], function() {
-  require(['../js/app'], function() {
-    console.log('ok');
-  });
-});
--- dist/deck.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Deck = function Deck() {
-  this.name = 'Default';
-  this.cards = [];
-};
-($traceurRuntime.createClass)(Deck, {add: function(card) {
-    this.cards.push(card);
-  }}, {});
