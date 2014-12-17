cc813290eafeb6ea6e5441cb39151470fdc89e39
Menencia <menencia@gmail.com>
2014-12-17 22:14:51 +0400

    Fix errors when launching

--- src/app.js
+++ src/app.js
@@ -4,171 +4,165 @@
  * App module
  * @type {object}
  */
-var app = angular.module('ff-tcg', ['ngRoute', 'ngCookies']);
-
-/**
- * Utils Service
- */
-app.service('Utils', Utils);
+var app = angular.module('ff-tcgs', ['ngRoute', 'ngCookies']);
 
 /**
  * Game Service
  */
-app.service('Game', Game);
+app.factory('Game', ['$rootScope', '$cookieStore', '$timeout', function ($rootScope, $cookieStore, $timeout) {
+    return new Game($rootScope, $cookieStore, $timeout);
+}]);
 
 /**
  * Routes logic
  */
 app.config(['$routeProvider',
-  function($routeProvider) {
+    function ($routeProvider) {
 
-    $routeProvider.
-    when('/game', {
-      templateUrl: 'partials/game.html',
-      controller: GameCtrl
-    }).
-    when('/play', {
-      templateUrl: 'partials/play.html',
-      controller: PlayCtrl
-    }).
-    when('/save', {
-      templateUrl: 'partials/save.html',
-      controller: SaveCtrl
-    }).
-    otherwise({
-      redirectTo: '/game'
-    });
-  }
+        $routeProvider.
+            when('/game', {
+                templateUrl: 'partials/game.html',
+                controller : 'GameCtrl'
+            }).
+            when('/play', {
+                templateUrl: 'partials/play.html',
+                controller : 'PlayCtrl'
+            }).
+            when('/save', {
+                templateUrl: 'partials/save.html',
+                controller : 'SaveCtrl'
+            }).
+            otherwise({
+                redirectTo: '/game'
+            });
+    }
 ]);
 
 /**
  * NAV
  */
 
-function NavCtrl($scope, $location, Game) {
+app.controller('IndexCtrl', function ($scope, $location, Game) {
 
-  $scope.isActive = function(route) {
-    return route === $location.path();
-  }
+    $scope.isActive = function (route) {
+        return route === $location.path();
+    };
 
-  /**
-   * Go to the game
-   */
-  $scope.game = function() {
-    $location.path("/game");
-  };
+    /**
+     * Go to the game
+     */
+    $scope.game = function () {
+        $location.path("/game");
+    };
 
-  /**
-   * Save the game
-   */
-  $scope.save = function(ev) {
-    $location.path("/save");
-  };
+    /**
+     * Save the game
+     */
+    $scope.save = function (ev) {
+        $location.path("/save");
+    };
 
-}
+});
 
 /**
  * /Game
  */
 
-function GameCtrl($rootScope, $cookieStore, $timeout, $location, Game, Utils) {
+app.controller('GameCtrl', function ($rootScope, $cookieStore, $timeout, $location, Game) {
 
-  // STEP 1
-  // Load saved game from COOKIE
+    // STEP 1
+    // Load saved game from COOKIE
 
-  var save = $cookieStore.get('game');
+    var save = $cookieStore.get('game');
 
-  // STEP 2
-  // Extend COOKIE with background information
+    // STEP 2
+    // Extend COOKIE with background information
 
-  // GAME
-  if (Game.loaded) {
-    return;
-  }
+    // GAME
+    if (Game.loaded) {
+        return;
+    }
 
-  Game.init($rootScope, $cookieStore, $timeout);
+    Game.init($rootScope, $cookieStore, $timeout);
 
-  $rootScope.play = function() {
-    Game.newPlay();
-    $location.path("/play");
-  }
+    $rootScope.play = function () {
+        Game.newPlay();
+        $location.path("/play");
+    }
 
-};
+});
 
 /**
  * /Play
  */
 
-function PlayCtrl($rootScope, $location, $cookieStore, $http, $timeout, Game) {
+app.controller('PlayCtrl', function ($rootScope, $location, $cookieStore, $http, $timeout, Game) {
 
-  /**
-   * Checkin'
-   */
-  if (!Game.loaded || Game.mode != 'play') {
-    $location.path("/game");
-    return;
-  }
+    /**
+     * Checkin'
+     */
+    if (!Game.loaded || Game.mode != 'play') {
+        $location.path("/game");
+        return;
+    }
 
-};
+});
 
 /**
  * /Save
  */
 
-function SaveCtrl($scope, $rootScope, $location, Game, Utils) {
+app.controller('SaveCtrl', function ($scope, $rootScope, $location, Game) {
 
-  /**
-   * Checkin'
-   */
-  if (!Game.loaded) {
-    $location.path("/game");
-    return;
-  }
-
-  /**
-   * Save the game
-   */
-  $rootScope.saveGame = function(ev) {
-    Game.save();
-    Utils.animate(ev, 'OK!');
-  };
-
-  /**
-   * Reset the game
-   */
-  $rootScope.resetGame = function(ev) {
-    if (confirm('Are you sure ? You\'ll lose everything !')) {
-      Game.reset();
-      location.reload();
+    /**
+     * Checkin'
+     */
+    if (!Game.loaded) {
+        $location.path("/game");
+        return;
     }
-  };
 
-  /**
-   * Export the current save
-   */
-  $rootScope.exportLastSave = function(ev) {
-    $scope.area = Game.last_export;
-    Utils.animate(ev, 'OK!');
-  };
+    /**
+     * Save the game
+     */
+    $rootScope.saveGame = function (ev) {
+        Game.save();
+    };
 
-  /**
-   * Export the current game
-   */
-  $rootScope.exportCurrentGame = function(ev) {
-    var save = Game.export();
-    $scope.area = JSON.stringify(save);
-    Utils.animate(ev, 'OK!');
-  };
+    /**
+     * Reset the game
+     */
+    $rootScope.resetGame = function (ev) {
+        if (confirm('Are you sure ? You\'ll lose everything !')) {
+            Game.reset();
+            location.reload();
+        }
+    };
 
-  /**
-   * Import a save
-   */
-  $rootScope.importSave = function(ev) {
-    if (!Game.last_export || confirm('Are you sure ? You\'ll lose your current save !')) {
-      var save = JSON.parse($scope.area);
-      Game.import(save);
-      location.reload();
-    }
-  };
+    /**
+     * Export the current save
+     */
+    $rootScope.exportLastSave = function (ev) {
+        $scope.area = Game.last_export;
+    };
 
-}
+    /**
+     * Export the current game
+     */
+    $rootScope.exportCurrentGame = function (ev) {
+        var save = Game.export();
+        $scope.area = JSON.stringify(save);
+    };
+
+    /**
+     * Import a save
+     */
+    $rootScope.importSave = function (ev) {
+        if (!Game.last_export || confirm('Are you sure ? You\'ll lose your current save !')) {
+            var save = JSON.parse($scope.area);
+            Game.import(save);
+            location.reload();
+        }
+    };
+
+});
