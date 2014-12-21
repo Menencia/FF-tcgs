0bcb7ace0e0ac1995c0a317942af360705b9b6f3
Menencia <menencia@gmail.com>
2014-12-21 23:40:22 +0400

    Fix Jecht (1) Card

--- src/cards/jecht_1.js
+++ src/cards/jecht_1.js
@@ -17,7 +17,7 @@ class Card_Jecht_1 extends Card {
                 cost  : [
                     {elt: "special", nbr: 1},
                     {elt: "fire", nbr: 1},
-                    {elt: "dark", nbr: 2},
+                    {elt: "light", nbr: 2},
                     {elt: "dull", nbr: 1}
                 ],
                 name  : 'Ultimate Jecht Shot',
